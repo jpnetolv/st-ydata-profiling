c601957b9bdc3445e010099c93d10a8d677c9e3d
jpnetolv <jpnetotest@gmail.com>
2024-12-17 18:28:21 -0300

    Documentação

--- st-tdt-profiling.py
+++ st-tdt-profiling.py
@@ -1,18 +1,32 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
-from utils import process_file
+# Importação das bibliotecas necessárias
+import pandas as pd  # Biblioteca para manipulação e análise de dados
+from ydata_profiling import ProfileReport  # Biblioteca para gerar relatórios detalhados de análise de dados
+import streamlit as st  # Biblioteca para criar aplicativos interativos de dados na web
+from streamlit_pandas_profiling import st_profile_report  # Integração entre Streamlit e ydata-profiling para exibir relatórios
+from utils import process_file  # Função personalizada para carregar arquivos CSV
 
+# Configuração do layout do Streamlit para exibição em larga escala
 st.set_page_config(layout='wide')
 
 def main():
+    """
+    Função principal que define a estrutura do aplicativo Streamlit para análise de dados com Pandas Profiling.
+
+    Esta função exibe um título, permite o upload de um arquivo CSV, e, se um arquivo for carregado,
+    exibe estatísticas básicas sobre o DataFrame, como o número de linhas e colunas, os tipos de dados das colunas,
+    e as primeiras linhas do DataFrame. Além disso, gera e exibe um relatório detalhado utilizando o ydata-profiling.
+    O usuário também pode baixar o relatório gerado como um arquivo HTML.
+
+    Caso nenhum arquivo seja carregado, exibe uma mensagem de alerta para que o usuário faça o upload de um arquivo CSV.
+    """
+    # Define o título do aplicativo
     st.title('Análise de Dados com Pandas Profiling')
 
+    # Chama a função process_file do utils.py para permitir o upload do arquivo
     df = process_file()
 
     if df is not None:
-        #exibir estatísticas básicas e pré-visualização dos dados lado a lado
+        # Se o DataFrame for carregado com sucesso, exibe informações básicas
         st.subheader('Visão Geral dos Dados')
 
         col1, col2, col3 = st.columns(3)
@@ -28,17 +42,20 @@ def main():
             st.write('**Primeiras linhas do DataFrame:**')
             st.write(df.head())
 
+        # Gera o relatório de Pandas Profiling
         st.title('Relatório Ydata Profiling')
         profile = ProfileReport(df, minimal=True)
         st_profile_report(profile)
         
-        #opção para baixar o relatório como HTML
+        # Opção para baixar o relatório como um arquivo HTML
         st.subheader('Baixar Relatório')
         html = profile.to_html()
         st.download_button(label="Baixar Relatório como HTML", data=html, file_name="relatorio_analise.html", mime="text/html")
         
     else:
+        # Exibe uma mensagem de alerta se nenhum arquivo for carregado
         st.warning("Nenhum arquivo carregado. Por favor, faça o upload de um arquivo CSV.")
 
+# Verifica se o script está sendo executado diretamente
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -2,10 +2,24 @@ import pandas as pd
 import streamlit as st
 
 def process_file():
+    """
+    Função para processar o arquivo CSV carregado pelo usuário.
+
+    Exibe um botão de upload de arquivo no Streamlit. Quando o usuário faz o upload de um arquivo CSV, 
+    o arquivo é lido utilizando o pandas e o DataFrame resultante é retornado. Se nenhum arquivo for 
+    carregado, a função retorna None.
+
+    Returns:
+        pd.DataFrame or None: Retorna o DataFrame carregado a partir do arquivo CSV, ou None se nenhum arquivo 
+                               for carregado.
+    """
+    # Exibe o botão para upload do arquivo CSV
     upl = st.file_uploader('Suba seu Arquivo CSV')
     
     if upl is not None:
+        # Se um arquivo for carregado, lê o CSV e retorna o DataFrame
         df = pd.read_csv(upl)
         return df
     else:
+        # Se nenhum arquivo for carregado, retorna None
         return None
